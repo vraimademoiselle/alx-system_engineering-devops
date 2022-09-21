7a02d05e7e987ae506f4e56df9105b53a899fba6
vraimademoiselle <mademoisellebone@gmail.com>
2022-09-21 10:56:11 +0300

    csecond file and readme

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# Installs puppet-lint
-
-package { 'puppet-lint':
-  ensure   => '2.5.1',
-  provider => 'gem',
+# Installing flask 
+package { 'flask':
+  ensure   => '2.1.0',
+  provider =>  'pip3',
 }
