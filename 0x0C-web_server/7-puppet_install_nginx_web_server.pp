7cb911320bebc7f90e4fcf68e99554022d2f3af6
vraimademoiselle <mademoisellebone@gmail.com>
2022-09-28 16:20:16 +0300

    corrections

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,33 @@
-# Installs a Nginx server
+# Install nginx web server and configure using puppet
 
-exec {'install':
-  provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/luischaparroc permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+include stdlib
+
+# Update apt
+exec { 'Update apt library':
+  command => 'sudo apt update',
+  path    => '/usr/bin/'
+}
+
+# Install nginx
+package { 'nginx':
+  ensure   => 'installed',
+}
+
+# Configure nginx
+file_line { 'Add redirect line':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'server_name _;',
+  line   => "location /redirect_me {\n\treturn 301 http://github.com/;\n}",
+}
+
+# Set index content
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+# Restart nginx service
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
